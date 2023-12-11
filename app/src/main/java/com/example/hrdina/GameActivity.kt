758072d71348674c9fba5ba8eccfa603c8c4742b
KunesTOP <125481995+KunesTOP@users.noreply.github.com>
2023-12-11 20:10:25 +0100

    Com.example.hra Activity layout graficka cast aplikace

---
+++ app/src/main/java/com/example/hrdina/GameActivity.kt
@@ -0,0 +1,22 @@
+package com.example.hra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.example.hrdina.databinding.ActivityGameBinding
+
+class GameActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityGameBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val heroName = intent.getStringExtra("heroName")
+        if (heroName != null) {
+            // Set the heroName to the TextView
+            binding.textHeroName.text = heroName
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hrdina/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.hra
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hrdina.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.newGameBtn.setOnClickListener {
+            startActivity(Intent(this, NewGameActivity::class.java))
+        }
+
+        binding.exitBtn.setOnClickListener {
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.addCategory(Intent.CATEGORY_HOME)
+            startActivity(intent)
+        }
+    }
+}
